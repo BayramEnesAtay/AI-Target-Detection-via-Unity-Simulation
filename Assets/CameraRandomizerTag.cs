055b070258a2bc971c0c4dee995e1781b8d795f8
Bayram Enes Atay <bayrameness4242@gmail.com>
2026-08-21 17:15:03 +0300

    upload the scripts

---
+++ Assets/CameraRandomizerTag.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.Perception.Randomization.Randomizers;
+using System;
+
+[AddComponentMenu("Perception/RandomizerTags/CameraRandomizerTag")]
+public class CameraRandomizerTag :RandomizerTag
+{
+    
+}
